dacfac9407313e909ab7dd36e716ce6c0f777e66
GauravSinghRauthan <gauravsingh199889@gmail.com>
2023-07-17 11:51:32 +0530

    new change

--- controller/controller.js
+++ controller/controller.js
@@ -4,7 +4,7 @@ const User = require('../model/userModel.js')
 exports.createUser=async (req,res)=>{
     try{
         const {user_name,user_email,user_password} = req.body
-        if(!email || !password || !name){
+        if(!user_email || !user_name || !user_password){
             res.status(400).json({
                 message: "all field required"
             })
@@ -29,14 +29,14 @@ exports.createUser=async (req,res)=>{
 exports.login = async (req,res)=>{
     try{
         const {user_email,user_password}= req.body
-        if(!email || !password){
+        if(!user_email || !user_password){
             res.status(400).json({
                 message: "all field required"
             })
         }
         const user = await User.findOne({email:user_email})
         if(user){
-            if(user.user_password===user_password){
+            if(user.password===user_password){
                 res.status(200).json({
                     success: true,
                     message: "successfuly login"
